cad4d7747855185dcfb10b1e28d0fb7ae57ec032
Deyvison Tavares <deyvisontav@gmail.com>
2022-07-07 15:37:47 -0300

    completed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ export function Input({ ...rest }) {
   return (
     <div>
       <input
-        className="w-full flex-1 mb-2 md:mb-3 p-3 rounded md:rounded-md border border-gray-300 focus-visible:border-blue-500
+        className="w-full flex-1 mb-2 md:mb-3 p-3 rounded md:rounded-md border border-gray-300   focus-visible:border-blue-500
         bg-gray-200 md:bg-white text-sm md:text-base placeholder-gray-600 md:placeholder-gray-500"
         {...rest}
       />
